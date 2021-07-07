f7d87502615c407cfd5e33cd95b0b30e93bc7d39
M1dnightNinja <wallenjos01@gmail.com>
2021-07-06 22:03:29 -0600

    Built with 1.17.1. Support Taterzens 1.2.0.

--- fabric/src/main/java/me/m1dnightninja/skinsetter/fabric/integragion/TaterzensIntegration.java
+++ fabric/src/main/java/me/m1dnightninja/skinsetter/fabric/integragion/TaterzensIntegration.java
@@ -8,7 +8,7 @@ import me.m1dnightninja.midnightcore.api.text.MComponent;
 import me.m1dnightninja.skinsetter.api.SkinSetterAPI;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
-import org.samo_lego.taterzens.interfaces.TaterzenEditor;
+import org.samo_lego.taterzens.interfaces.ITaterzenEditor;
 import org.samo_lego.taterzens.npc.TaterzenNPC;
 
 import java.util.UUID;
@@ -17,7 +17,7 @@ public class TaterzensIntegration {
 
     public static MComponent setNPCSkin(ServerPlayer player, Skin skin) {
 
-        TaterzenEditor editor = (TaterzenEditor) player;
+        ITaterzenEditor editor = (ITaterzenEditor) player;
         UUID u = player.getUUID();
 
         MPlayer pl = MidnightCoreAPI.getInstance().getPlayerManager().getPlayer(u);
